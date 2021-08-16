df141768c3c681161d0a821c32ff8d7fc429fb30
Ilya <kiselev_i@garpix.com>
2021-08-16 18:33:42 +0300

    SHIP-6. Добавлены владельцы ресурсов, имплементирован UserInterface

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -4,12 +4,13 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\Table(name="`user`")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id
