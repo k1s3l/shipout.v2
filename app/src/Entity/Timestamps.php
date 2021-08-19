170c0b11cd27f15aafe2b9fc3052d766c9b74701
K1S3L <kis3lewil@yandex.ru>
2021-08-19 22:17:31 +0300

    SHIP-3. add trait, add fixtures

---
+++ app/src/Entity/Timestamps.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait Timestamps
+{
+    /**
+     * @ORM\Column(type="datetime", nullable=false)
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=false)
+     */
+    private $updated_at;
+
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @ORM\PrePersist()
+     */
+    public function setCreatedAt(): self
+    {
+        $this->created_at = new \DateTime();
+        $this->updated_at = new \DateTime();
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * @ORM\PreUpdate()
+     */
+    public function setUpdatedAt(): self
+    {
+        $this->updated_at = new \DateTime();
+
+        return $this;
+    }
+}
